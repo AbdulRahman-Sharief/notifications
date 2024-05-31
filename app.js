b4590928cb93a78a4cd700f0ff210393b477a0da
AbdulRahman-Sharief <abdulrahmansharief2@gmail.com>
2024-05-31 23:57:58 +0300

    cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 //BODY PARSER || READ DATA FROM BODY TO req.body
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-
+app.use(cors("*"));
 const notificationsController = require("./controller/notifications.controller");
 app.post("/getAll", notificationsController.getAll);
 app.post("/getOne", notificationsController.getOne);
